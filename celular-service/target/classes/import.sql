b64fc88e576ca4cd4ebf29d09dae59d079ee8f16
GersainLeal <gersain.leal02@uceva.edu.co>
2024-09-02 00:46:49 -0500

    Add files via upload

---
+++ celular-service/target/classes/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO celular (marca, modelo, precio, capacidad) VALUES ('Apple', 'iPhone 14', 999, 128);
+INSERT INTO celular (marca, modelo, precio, capacidad) VALUES ('Samsung', 'Galaxy S23', 849, 256);
+INSERT INTO celular (marca, modelo, precio, capacidad) VALUES ('Xiaomi', 'Mi 13', 699, 128);
+INSERT INTO celular (marca, modelo, precio, capacidad) VALUES ('Google', 'Pixel 7', 599, 128);
+INSERT INTO celular (marca, modelo, precio, capacidad) VALUES ('OnePlus', '9 Pro', 729, 256);
+INSERT INTO celular (marca, modelo, precio, capacidad) VALUES ('Sony', 'Xperia 1 V', 899, 512);
